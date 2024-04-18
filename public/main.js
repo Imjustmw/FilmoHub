5a949e4e63ded71bfdfd21599f6d1968eacc8f79
Jonathan Joseph <jonathan.joseph2@my.uwi.edu>
2024-04-18 20:53:06 +0000

    I believe this is the working non broken script...hopefully...

--- public/main.js
+++ public/main.js
@@ -18,80 +18,98 @@ async function init_Home() {
   let retryCount = 0;
 
   try {
-    // Top 5 Latest
-    let new_result1 = await sendRequest('MDB', 'list/movies/1'); // latest 20 movies
-    let top5 = new_result1.finalResponse.results.slice(0, 5);
-    for (let movie of top5) {
-      let fullMovie = await sendRequest('MDB', `description/movies?movieId=${movie.movieId}`);
-      fullMovie.result.trailer = '';
-      try {
-        fullMovie.result.trailer = getTrailer(fullMovie.result.videos.results);
-      } catch (e) {
-        console.error(e);
-      }
-      
-      HomePage.Top.list.push(fullMovie.result);
-    }
-    // Top Latest
-    HomePage.New.list.push(...new_result1.finalResponse.results.slice(5)); // remaining latest goes in new container
+      // Top 5 Latest
+      let new_result1 = await sendRequest('MDB', 'list/movies/1'); // latest 20 movies
+      let top5 = new_result1.finalResponse.results.slice(0, 5);
+      for (let movie of top5) {
+          let fullMovie = await sendRequest('MDB', `description/movies?movieId=${movie.movieId}`);
+          fullMovie.result.trailer = '';
+          try {
+              fullMovie.result.trailer = getTrailer(fullMovie.result.videos.results);
+          } catch (e) {
+              console.error(e);
+          }
 
-    let new_result2 = await sendRequest('MDB', 'list/movies/2');
-    retryCount = 0;
-    while (retryCount < retries) {
-      if (!new_result2.finalResponse) {
-        retryCount++;
-        await new Promise(resolve => setTimeout(resolve, timeout));
+          HomePage.Top.list.push(fullMovie.result);
       }
-    }
-    HomePage.New.list.push(...new_result2.finalResponse.results); // add another page of new movies to new container
+      // Top Latest
+      HomePage.New.list.push(...new_result1.finalResponse.results.slice(5)); // remaining latest goes in new container
 
-    // Upcoming Movies
-    let upcoming_result1 = await sendRequest('MDB', 'upcoming/movies?page=1');
-    retryCount = 0;
-    while (retryCount < retries) {
-      if (!upcoming_result1.finalResponse) {
-        retryCount++;
-        await new Promise(resolve => setTimeout(resolve, timeout));
+      let new_result2;
+      retryCount = 0;
+      while (retryCount < retries) {
+          new_result2 = await sendRequest('MDB', 'list/movies/2');
+          if (!new_result2.finalResponse) {
+              retryCount++;
+              await new Promise(resolve => setTimeout(resolve, timeout));
+          } else {
+              break;
+          }
       }
-    }
-    HomePage.Upcoming.list.push(...upcoming_result1.finalResponse.results);
-    let upcoming_result2 = await sendRequest('MDB', 'upcoming/movies?page=2');
-    retryCount = 0;
-    while (retryCount < retries) {
-      if (!upcoming_result2.finalResponse) {
-        retryCount++;
-        await new Promise(resolve => setTimeout(resolve, timeout));
-      }
-    }
-    HomePage.Upcoming.list.push(...upcoming_result2.finalResponse.results);
+      HomePage.New.list.push(...new_result2.finalResponse.results); // add another page of new movies to new container
 
-    // Top Rated Movies
-    let toprated_result1 = await sendRequest('MDB', 'toprated/movies?page=1');
-    retryCount = 0;
-    while (retryCount < retries) {
-      if (!toprated_result1.finalResponse) {
-        retryCount++;
-        await new Promise(resolve => setTimeout(resolve, timeout));
+      // Upcoming Movies
+      let upcoming_result1;
+      retryCount = 0;
+      while (retryCount < retries) {
+          upcoming_result1 = await sendRequest('MDB', 'upcoming/movies?page=1');
+          if (!upcoming_result1.finalResponse) {
+              retryCount++;
+              await new Promise(resolve => setTimeout(resolve, timeout));
+          } else {
+              break;
+          }
       }
-    }
-    HomePage.TopRated.list.push(...toprated_result1.finalResponse.results);
-    let toprated_result2 = await sendRequest('MDB', 'toprated/movies?page=2');
-    retryCount = 0;
-    while (retryCount < retries) {
-      if (!toprated_result2.finalResponse) {
-        retryCount++;
-        await new Promise(resolve => setTimeout(resolve, timeout));
-      }
-    }
-    HomePage.TopRated.list.push(...toprated_result2.finalResponse.results);
+      HomePage.Upcoming.list.push(...upcoming_result1.finalResponse.results);
 
-    // My List (User's Favourites)
+      let upcoming_result2;
+      retryCount = 0;
+      while (retryCount < retries) {
+          upcoming_result2 = await sendRequest('MDB', 'upcoming/movies?page=2');
+          if (!upcoming_result2.finalResponse) {
+              retryCount++;
+              await new Promise(resolve => setTimeout(resolve, timeout));
+          } else {
+              break;
+          }
+      }
+      HomePage.Upcoming.list.push(...upcoming_result2.finalResponse.results);
+
+      // Top Rated Movies
+      let toprated_result1;
+      retryCount = 0;
+      while (retryCount < retries) {
+          toprated_result1 = await sendRequest('MDB', 'toprated/movies?page=1');
+          if (!toprated_result1.finalResponse) {
+              retryCount++;
+              await new Promise(resolve => setTimeout(resolve, timeout));
+          } else {
+              break;
+          }
+      }
+      HomePage.TopRated.list.push(...toprated_result1.finalResponse.results);
+
+      let toprated_result2;
+      retryCount = 0;
+      while (retryCount < retries) {
+          toprated_result2 = await sendRequest('MDB', 'toprated/movies?page=2');
+          if (!toprated_result2.finalResponse) {
+              retryCount++;
+              await new Promise(resolve => setTimeout(resolve, timeout));
+          } else {
+              break;
+          }
+      }
+      HomePage.TopRated.list.push(...toprated_result2.finalResponse.results);
+
+      // My List (User's Favourites)
   } catch (e) {
-    console.error('Error loading Home:', e)
+      console.error('Error loading Home:', e)
   }
   //display Home
   displayHome();
 }
+
 
 async function init_Genres() {
 
